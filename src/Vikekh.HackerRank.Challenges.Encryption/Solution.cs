119c4a4a05b66913c71d1beebf138539dfae626b
Viktor Ekholm <vieekk@gmail.com>
2016-01-22 21:55:39 +0100

    Create project for implementation

---
+++ src/Vikekh.HackerRank.Challenges.Encryption/Solution.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Vikekh.HackerRank.Challenges.Encryption
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/encryption
+    /// </summary>
+    public class Solution
+    {
+        public static void Main(String[] args)
+        {
+            var message = Console.ReadLine();
+
+            Console.WriteLine(Encrypt(message));
+        }
+
+        public static string Encrypt(string message)
+        {
+            var length = message.Length;
+            var lengthSqrt = Math.Sqrt(length);
+            var lengthSqrtFloor = (int)Math.Floor(lengthSqrt);
+            var lengthSqrtCeiling = (int)Math.Ceiling(lengthSqrt);
+            var minArea = int.MaxValue;
+            var rows = 0;
+            var columns = 0;
+            var encryptedMessage = string.Empty;
+
+            for (var i = lengthSqrtFloor; i <= lengthSqrtCeiling; i++)
+            {
+                for (var j = i; j <= lengthSqrtCeiling; j++)
+                {
+                    var area = i * j;
+
+                    if (area >= length && area < minArea)
+                    {
+                        rows = i;
+                        columns = j;
+                    }
+                }
+            }
+
+            for (var i = 0; i < columns; i++)
+            {
+                for (var j = 0; j < rows; j++)
+                {
+                    var index = i + j * columns;
+
+                    if (index < length)
+                    {
+                        encryptedMessage += message[index];
+                    }
+                }
+
+                if (i < columns - 1)
+                {
+                    encryptedMessage += " ";
+                }
+            }
+
+            return encryptedMessage;
+        }
+    }
+}
