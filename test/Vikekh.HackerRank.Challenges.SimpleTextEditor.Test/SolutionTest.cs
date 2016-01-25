6ff0a1e5222b1b217f66cc6d7cd9152690d0368c
Viktor Ekholm <vieekk@gmail.com>
2016-01-25 22:10:03 +0100

    Empty test project

---
+++ test/Vikekh.HackerRank.Challenges.SimpleTextEditor.Test/SolutionTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vikekh.HackerRank.Challenges.SimpleTextEditor.Test
+{
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
