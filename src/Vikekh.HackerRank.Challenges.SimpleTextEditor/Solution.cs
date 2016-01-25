43502a9eae7b5e0027ebb2313087c198ed870fd6
Viktor Ekholm <vieekk@gmail.com>
2016-01-25 22:06:21 +0100

    Add empty project

---
+++ src/Vikekh.HackerRank.Challenges.SimpleTextEditor/Solution.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Vikekh.HackerRank.Challenges.SimpleTextEditor
+{
+    public class Solution
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
